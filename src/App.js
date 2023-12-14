c648a10eff77cab40d60d800234d214fe80f20ab
Owes Ansari <Owes.Ansari@teknopoint.local>
2023-12-14 18:41:29 +0530

    react-random-color

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './App.css'
+
+const App = () => {
+
+  const [randomColor , setRanodmColor] = useState('16777215');
+
+  const handleClick = () => {
+    const randomNumber = Math.floor(Math.random() * 16777215);
+    const randomCode = randomNumber.toString(16)
+    // console.log(randomNumber,'randomNumber');
+    // console.log(randomCode,'randomCode');
+    setRanodmColor(randomCode);
+  }
+
+  return (
+    <>
+      {/* <div className="main" style={`background : {#${randomColor}}`}> */}
+      <div className="main" style={{background : `#${randomColor}`}}>
+        <div className="box">
+          <h2 id="colorId"></h2>
+          <button id="btn" onClick={handleClick}>Click Here</button>
+          <div className="imgPar">
+            <img className="clipboard" src="copyIcon.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
